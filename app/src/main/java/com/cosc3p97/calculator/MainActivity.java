0a72862d7c709ab2b1865914d5fa03d7f531e3ef
Yuvraj <17yuvraj.sehgal@gmail.com>
2024-10-16 16:49:46 -0400

    basic operations working

--- app/src/main/java/com/cosc3p97/calculator/MainActivity.java
+++ app/src/main/java/com/cosc3p97/calculator/MainActivity.java
@@ -1,16 +1,95 @@
 package com.cosc3p97.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText resultView, newNumberView;
+    private TextView operationView;
+    private Double operand1, operand2;
+    private String pendingOperation = "=";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        resultView = (EditText) findViewById(R.id.result);
+        newNumberView = (EditText) findViewById(R.id.newNumber);
+        operationView = findViewById(R.id.operation);
+
+
+        Button button0 = (Button) findViewById(R.id.button0);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+        Button button7 = (Button) findViewById(R.id.button7);
+        Button button8 = (Button) findViewById(R.id.button8);
+        Button button9 = (Button) findViewById(R.id.button9);
+
+        Button buttonDecimal = (Button) findViewById(R.id.buttonDecimal);
+        Button buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        Button buttonSub = (Button) findViewById(R.id.buttonSub);
+        Button buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
+        Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
+        Button buttonEquals = (Button) findViewById(R.id.buttonEquals);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                newNumberView.append(button.getText().toString());
+            }
+        };
+
+        button0.setOnClickListener(listener);
+        button1.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+        button3.setOnClickListener(listener);
+        button4.setOnClickListener(listener);
+        button5.setOnClickListener(listener);
+        button6.setOnClickListener(listener);
+        button7.setOnClickListener(listener);
+        button8.setOnClickListener(listener);
+        button9.setOnClickListener(listener);
+        buttonDecimal.setOnClickListener(listener);
+
+
+        View.OnClickListener operationListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                String operation = button.getText().toString();
+                String value = newNumberView.getText().toString();
+                if (value.length() != 0) {
+                    performOperation(value, operation);
+                }
+                pendingOperation = operation;
+                operationView.setText(pendingOperation);
+            }
+        };
+
+        buttonAdd.setOnClickListener(operationListener);
+        buttonSub.setOnClickListener(operationListener);
+        buttonMultiply.setOnClickListener(operationListener);
+        buttonDivide.setOnClickListener(operationListener);
+        buttonEquals.setOnClickListener(operationListener);
     }
+
+    private void performOperation(String value, String operation) {
+        operationView.setText(operation);
+    }
+
 }
